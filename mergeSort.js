dd66ae9abf5bf1c30a3e2a8a1ee99b7caa3f5d3b
marcorafael.mucho-12@cpu.edu.ph <muchmarc10@gmail.com>
2022-02-20 13:27:02 +0800

    Add merge sort code

--- mergeSort.js
+++ mergeSort.js
@@ -1,52 +1,70 @@
 import * as fs from 'fs'
 
-const insertionSort = (array) => {
-    for (let i = 1; i < array.length; i++) {
-        for (let k = i; k > 0 && array[k] < array[k - 1]; k--) {
-            const current = array[k]
-            array[k] = array[k - 1]
-            array[k-1] = current
-        }
+const mergeSort = (array) => {
+    if (array.length <= 1) {
+        return array
     }
+
+    const middle = Math.floor(array.length / 2)
+    const left = array.slice(0, middle)
+    const right = array.slice(middle)
+
+    const compareLeftRight = (leftArray, rightArray) => {
+        const outputArray = []
+
+        while (leftArray.length && rightArray.length) {
+            if (leftArray[0]< rightArray[0]) {
+                outputArray.push(leftArray.shift())
+            } else {
+                outputArray.push(rightArray.shift())
+            }
+        }
+
+        return outputArray.concat(leftArray).concat(rightArray)
+    }
+
+    return compareLeftRight(mergeSort(left), mergeSort(right))
 }
 
-const size = 60_000
+const size = 15_000
 const numberArray = fs.readFileSync('numberData.txt').toString().trim().split('\n').slice(0, size).map(n => Number(n))
 const stringArray = fs.readFileSync('stringData.txt').toString().trim().split('\n').slice(0, size)
 
 //NUMBER ARRAY
 console.log('------------------------------------------------------------------------------------')
 const numberAverageStart = Date.now()
-insertionSort(numberArray)
+mergeSort(numberArray)
 const numberAverageEnd = Date.now()
 console.log(`Array data: Numbers\nArray size: ${size}\nCase: Average\nElapsed time: ${ (numberAverageEnd - numberAverageStart) / 1000 }s\n`)
 
+const numberBest = mergeSort(numberArray)
 const numberBestStart = Date.now()
-insertionSort(numberArray)
+mergeSort(numberBest)
 const numberBestEnd = Date.now()
 console.log(`Array data: Numbers\nArray size: ${size}\nCase: Best\nElapsed time: ${ (numberBestEnd - numberBestStart) / 1000 }s\n`)
 
-numberArray.reverse()
+const numberWorst = mergeSort(numberArray).reverse()
 const numberWorstStart = Date.now()
-insertionSort(numberArray)
+mergeSort(numberWorst)
 const numberWorstEnd = Date.now()
 console.log(`Array data: Numbers\nArray size: ${size}\nCase: Worst\nElapsed time: ${ (numberWorstEnd - numberWorstStart) / 1000 }s`)
 
 //STRING ARRAY
 console.log('------------------------------------------------------------------------------------')
 const stringAverageStart = Date.now()
-insertionSort(stringArray)
+mergeSort(stringArray)
 const stringAverageEnd = Date.now()
 console.log(`Array data: String\nArray size: ${size}\nCase: Average\nElapsed time: ${ (stringAverageEnd - stringAverageStart) / 1000 }s\n`)
 
+const stringBest = mergeSort(stringArray)
 const stringBestStart = Date.now()
-insertionSort(stringArray)
+mergeSort(stringBest)
 const stringBestEnd = Date.now()
 console.log(`Array data: String\nArray size: ${size}\nCase: Best\nElapsed time: ${ (stringBestEnd - stringBestStart) / 1000 }s\n`)
 
-stringArray.reverse()
+const stringWorst = mergeSort(stringArray).reverse()
 const stringWorstStart = Date.now()
-insertionSort(stringArray)
+mergeSort(stringWorst)
 const stringWorstEnd = Date.now()
 console.log(`Array data: String\nArray size: ${size}\nCase: Worst\nElapsed time: ${ (stringWorstEnd - stringWorstStart) / 1000 }s`)
 console.log('------------------------------------------------------------------------------------')
